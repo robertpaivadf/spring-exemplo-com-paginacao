cb08739a73565b366827fd49c0f3c1a7e125a767
robertpaivadf <robertpaiva.df@gmail.com>
2023-03-01 22:46:41 -0300

    Validando regras de negócio usando o JPA

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -23,6 +23,15 @@ public class ParkingSpotController {
 
         @PostMapping
         public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDTO parkingSpotDto){
+                if(parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())){
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
+                }
+                if(parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())){
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
+                }
+                if(parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())){
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartment/block!");
+                }
                 var parkingSpotModel = new ParkingSpotModel();
                 BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel); //converter o DTO para Model
                 parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
